461626b670c6aee01801c47c3b0a53f157144cd4
Mohammad-Safari <msafari8051@gmail.com>
2024-07-27 02:37:22 +0330

    etc

--- unconditional_generation/train.sh
+++ unconditional_generation/train.sh
@@ -11,3 +11,8 @@ python train.py --data_path data/MS_COCO --maxlen 15 --save ./results/coco_resul
 python train.py --data_path data/NewsData --maxlen 32 --save ./results/news_result \
 --batch_size 256 --niters_gan_d 1 --niters_gan_ae 1 --lr_gan_g 4e-04 --lr_ae 0.27 \
 --add_noise --gan_d_local --enhance_dec
+
+# WMTNews Data
+python train.py --data_path data/PersianData --maxlen 32 --save ./results/persian_data \
+--batch_size 256 --niters_gan_d 1 --niters_gan_ae 1 --lr_gan_g 4e-04 --lr_ae 0.27 \
+--add_noise --gan_d_local --enhance_dec
